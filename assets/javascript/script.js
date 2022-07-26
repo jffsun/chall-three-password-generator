4dd767c0c5c2a4512593e8f31d88dcb650dcbf82
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2022-07-25 17:43:33 -0700

    invalid length functionality, README, images

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,30 +4,30 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
-// Ask length password
-var passwordLength = window.prompt("How many characters would you like in your password?\n*Must be at least 8 characters and no more than 128 characters*");
-  
+  // Ask passwordLength
+  var passwordLength = window.prompt("How many characters would you like in your password?\n*Must be at least 8 characters and no more than 128 characters*");
+
   // Password length requirement 
   while (passwordLength < 8 || passwordLength > 128) {
+      
+    // Incorrect length alert. Retry until correct
+     var passwordLength = window.prompt("ERROR: Too few or too many characters. Try Again.\n*Must be at least 8 characters and no more than 128 characters*");
+      if (passwordLength >= 8 || passwordLength <= 128) {
+        continue;
+      }
+    }
 
-    // Incorrect length alert
-    window.alert("Password must be at least 8 characters and no more than 128 characters.");
-
-    // TO DO: Continue prompt until correct
-    var askLength = window.prompt("How many characters would you like in your password?\n*Must be at least 8 characters and no more than 128 characters*");
-  }
-
-  // Array to hold all possible characters in user's password
+  // Array to hold all possible characters in user password
   var masterString = []
 
   // Ask lowercase characters
   var lowerChars = "abcdefghijklmnopqrstuvwxyz";
 
-  // Split to separate lowerChars string into array
+  // Split to separate lowerChars string into one array
   lowerCharsSplit = lowerChars.split('');
-  askLower = window.confirm("Press OK if you would like lower characters in your password.");
+  askLower = window.confirm("Press OK if you would like lowercase characters in your password.");
 
-    // If "OK" pressed, add to masterString array
+    // If "OK" pressed, add array to masterString array
     if (askLower) {
       masterString.push(lowerCharsSplit)
     }
@@ -35,7 +35,7 @@ var passwordLength = window.prompt("How many characters would you like in your p
   // Ask uppercase characters
   var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   upperCharsSplit = upperChars.split('');
-  askUpper = window.confirm("Press OK if you would like upper characters in your password.");
+  askUpper = window.confirm("Press OK if you would like uppercase characters in your password.");
     if (askUpper) {
       masterString.push(upperCharsSplit)
     }
@@ -52,11 +52,11 @@ var passwordLength = window.prompt("How many characters would you like in your p
   var specialChars = "!@#$%^&*()"
   specialCharsSplit = specialChars.split('');
   askSpecial = window.confirm("Press OK if you would like special characters in your password.");
-    if (askNum) {
+    if (askSpecial) {
       masterString.push(specialCharsSplit)
     }
 
-  // Concatenates all arrays into one array
+  // Concatenates all arrays in masterString into one array
   masterString = masterString.flat()
 
   var password = generatePassword(passwordLength, masterString);
@@ -68,7 +68,7 @@ var passwordLength = window.prompt("How many characters would you like in your p
 
     var result = ""
 
-    // For loop to itterate character selection until password length met
+    // For loop itterating random character selection until password length met
     for (let i=0; i < passwordLength; i++) {
        var charIndex = (Math.floor(Math.random() * masterString.length))
        result += masterString[charIndex]
@@ -76,8 +76,7 @@ var passwordLength = window.prompt("How many characters would you like in your p
     
   var passwordText = document.querySelector("#password");
   passwordText.value = result;
-
-};
+  };
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
